59c6e58d881d6c7324b6a02721e54f5ae2ec764a
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-07-16 17:17:48 +0300

    adding programm with For

---
+++ src/Example/Fortest.java
@@ -0,0 +1,17 @@
+package Example;
+
+/*
+   Продемонстрировать применение цикла for .
+   Присвоить исходному файлу имя "ForTest . java"
+
+*/
+
+public class Fortest {
+    public static void main(String[] args) {
+        int x;
+
+        for (x = 0; x < 10; x = x + 1)
+            System.out.println("Значение x: " + x );
+    }
+
+}
