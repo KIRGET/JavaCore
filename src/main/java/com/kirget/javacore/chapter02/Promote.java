5327e8c89f8c1de465d0dd21739a828c6063a517
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-07-18 15:58:08 +0300

    type promotion

---
+++ src/main/java/com/kirget/javacore/chapter02/Promote.java
@@ -0,0 +1,20 @@
+package com.kirget.javacore.chapter02;
+
+// продвижение типа
+
+public class Promote {
+    public static void main(String[] args) {
+        byte b = 42;
+        char c = 'a';
+        short s = 1024;
+        int i = 50000;
+        float f = 5.67f;
+        double d = 1234;
+        double result = ( f * b) + ( i / c) - ( d * s);
+        System.out.println (( f * b ) + " + "+ (i / c) + " - " + ( d * s ));
+        System.out.println("result = " + result);
+
+    }
+}
+
+
