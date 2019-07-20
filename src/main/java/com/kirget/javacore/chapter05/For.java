f96d1f28480d69fc79902e3cdb9137134130ea95
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-07-20 22:06:03 +0300

    fix file

--- src/main/java/com/kirget/javacore/chapter05/For.java
+++ src/main/java/com/kirget/javacore/chapter05/For.java
@@ -2,8 +2,7 @@ package com.kirget.javacore.chapter05;
 
 public class For {
     public static void main(String[] args) {
-        int i = 0;
-        for (i=0; i<10; i++){
+        for (int i=0; i<10; i++){
             System.out.println(" Work "+ i);
         }
     }
