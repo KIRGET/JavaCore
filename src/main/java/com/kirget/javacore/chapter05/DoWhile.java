1715ff3d95bc8f2e9ccd3fe4cdf1168309c3e46b
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-07-20 21:37:44 +0300

    prgm with DoWhile

---
+++ src/main/java/com/kirget/javacore/chapter05/DoWhile.java
@@ -0,0 +1,13 @@
+package com.kirget.javacore.chapter05;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int n = 10;
+
+        do {
+            System . out . println ( " тaкт " + n ) ;
+            n--;
+        } while (n > 0 );
+
+    }
+}
