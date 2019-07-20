ab195e7be720e81f3adda64e7cfb23fad9be9566
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-07-20 21:09:27 +0300

    prgm with While

---
+++ src/main/java/com/kirget/javacore/chapter05/While.java
@@ -0,0 +1,13 @@
+package com.kirget.javacore.chapter05;
+
+public class While {
+    public static void main(String[] args) {
+        int value = 0;
+        while (value<5){
+            System.out.println( " Hi, am work ! " + value);
+            value = value+1;
+
+        }
+
+    }
+}
