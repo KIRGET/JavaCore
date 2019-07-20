a008d8b94dc599c3bce162d409dfb10e0c9496f1
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-07-20 19:11:35 +0300

    programm with if

---
+++ src/main/java/com/kirget/javacore/chapter05/ifElse.java
@@ -0,0 +1,16 @@
+package com.kirget.javacore.chapter05;
+
+public class ifElse {
+    public static void main(String[] args) {
+        int myInt = 10;
+        if (myInt<5) {
+            System.out.println(" Да верно ");
+        }
+        else if(myInt>15){
+            System.out.println(" Нет не верно ");
+        }
+        else{
+            System.out.println(" Ни один из вышеуказанных");
+        }
+    }
+}
