57af2f94a955a715a51793621902223347676ef6
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-07-20 21:59:44 +0300

    prgm with For

---
+++ src/main/java/com/kirget/javacore/chapter05/For.java
@@ -0,0 +1,10 @@
+package com.kirget.javacore.chapter05;
+
+public class For {
+    public static void main(String[] args) {
+        int i = 0;
+        for (i=0; i<10; i++){
+            System.out.println(" Work "+ i);
+        }
+    }
+}
