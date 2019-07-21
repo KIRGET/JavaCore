52a120606f500409444be4078e0e3728d5749952
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-07-21 22:07:49 +0300

    prgm with class Box

---
+++ src/main/java/com/kirget/javacore/chcapter06/Box.java
@@ -0,0 +1,26 @@
+package com.kirget.javacore.chcapter06;
+
+// Присвоить исходному файлу имя BoxDemo.java
+
+    class Box {
+    double width;
+    double heigth;
+    double depth;
+}
+
+ class BoxDemo {
+     public static void main(String[] args) {
+         Box mybox = new Box();
+         double vol;
+                   // присвоить значение переменным экземпляра mуЬох
+
+         mybox.width = 10;
+         mybox.heigth = 20;
+         mybox.depth = 15;
+         // рассчитать объем параллелепипеда
+         vol = mybox.width * mybox.heigth * mybox.depth;
+         System.out.println(" Обьём равен " + vol);
+     }
+ }
+
+
