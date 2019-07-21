8a6bd23c5254ae8c2b2160cc84a40259bdfe2eb5
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-07-21 16:26:52 +0300

    prgm with Search

--- src/main/java/com/kirget/javacore/chapter05/Search.java
+++ src/main/java/com/kirget/javacore/chapter05/Search.java
@@ -1,4 +1,21 @@
 package com.kirget.javacore.chapter05;
 
 public class Search {
+    public static void main(String[] args) {
+        int nums[] = {6,8,5,9,3,10,4,1,};
+        int val = 5;
+        boolean found = false;
+
+        // исполь зовать цикл for в стиле for each для
+        //поиска значения переменной val в массиве nums
+
+        for (int x : nums) {
+            if (x == val) {
+                found = true;
+                break;
+            }
+        }
+        if (found)
+            System.out.println(" Значение найдено");
+    }
 }
