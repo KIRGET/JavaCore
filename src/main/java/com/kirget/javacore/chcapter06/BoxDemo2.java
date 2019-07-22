055ca5e408dbcd13c2c7d056d92d231e0b0002a7
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-07-22 19:32:33 +0300

    prgm with two odject Box

---
+++ src/main/java/com/kirget/javacore/chcapter06/BoxDemo2.java
@@ -0,0 +1,31 @@
+package com.kirget.javacore.chcapter06;
+
+// В этой программе объявляются два объекта класса Вох
+
+
+public class BoxDemo2 {
+    public static void main(String[] args) {
+        Box mybox1 = new Box();
+        Box mybox2 = new Box();
+        double vol;
+
+        // присвоить значения переменным э кземпляра myЬox1
+        mybox1.width = 10;
+        mybox1.heigth = 15;
+        mybox1.depth = 20;
+
+        // присвоить другие значения переменным экземпляра mybox2
+        mybox2.width = 3;
+        mybox2.heigth = 6;
+        mybox2.depth = 9;
+
+        // рассчитать объем первого параллелепипеда
+        vol = mybox1.width * mybox1.heigth * mybox1.depth;
+        System.out.println("Обьём равен " + vol);
+
+        // рассчитать объем второго параллелепипеда
+        vol = mybox2.width * mybox2.heigth * mybox2.depth;
+        System.out.println("Обьём равен " + vol);
+
+    }
+}
