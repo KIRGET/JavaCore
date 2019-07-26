9818ac4e3c827723692543a7a071ef6ab887e5d0
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-07-26 16:36:27 +0300

    passing arguments by value

---
+++ src/main/java/com/kirget/javacore/chapter07/CallByValue.java
@@ -0,0 +1,21 @@
+package com.kirget.javacore.chapter07;
+
+// Аргументы примитивных типов передаются по значению
+
+class  Test1 {
+    void meth (int i, int j) {
+        i *= 2;
+        j /= 2;
+    }
+}
+
+public class CallByValue {
+    public static void main(String[] args) {
+        Test1 ob = new Test1();
+        int a = 15, b = 20;
+        System.out.println (" a и b до вызова " + a + " " + b);
+
+        ob.meth (a,b);
+        System.out.println (" a и b после вызова " + a + " " + b);
+    }
+}
