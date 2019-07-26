607d0b262ed0f1927cf5f267893dd44fc88240e9
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-07-26 17:03:26 +0300

    Transfer of objects by reference to them

---
+++ src/main/java/com/kirget/javacore/chapter07/PassObLink.java
@@ -0,0 +1,30 @@
+package com.kirget.javacore.chapter07;
+
+// Передача объектов по ссылке на них
+
+class Test2 {
+    int a, b;
+
+    Test2 (int i, int j) {
+       a = i;
+       b = j;
+    }
+
+    // Передать объект
+
+    void meth (Test2 o) {
+        o.a *=2;
+        o.b /=2;
+    }
+}
+
+public class PassObLink {
+    public static void main(String[] args) {
+        Test2 ob = new Test2 (15,20);
+        System.out.println("ob.a и ob.b до вызова: " + ob.a + " " + ob.b);
+
+        ob.meth(ob);
+        System.out.println("ob.a и ob.b после вызова " + ob.a + " " + ob.b);
+
+    }
+}
