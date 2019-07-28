bab0df5703d66e7f46d6e2f245bf95fc1bc602f9
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-07-28 17:27:51 +0300

    used string

---
+++ src/main/java/com/kirget/javacore/chapter07/StringDemo.java
@@ -0,0 +1,15 @@
+package com.kirget.javacore.chapter07;
+
+// применение символьных строк
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String strOb1 = "первая сторка";
+        String strOb2 = "вторая сторка";
+        String strOb3 = strOb1 + " и " + strOb2;
+
+        System.out.println(strOb1);
+        System.out.println(strOb2);
+        System.out.println(strOb3);
+    }
+}
