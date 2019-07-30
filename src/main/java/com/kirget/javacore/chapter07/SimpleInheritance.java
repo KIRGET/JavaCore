589feaeff62ce7797790d0b1149c033d3b0d3fc2
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-07-30 17:29:02 +0300

    Inheritance

---
+++ src/main/java/com/kirget/javacore/chapter07/SimpleInheritance.java
@@ -0,0 +1,54 @@
+package com.kirget.javacore.chapter07;
+
+// Пример Наследования ( SimpleInheritance );
+
+class A {
+    int i,j;
+
+    void showij () {
+        System.out.println(" i и j :" + i + " " + j);
+
+    }
+}
+ // создать подкласс B путем расширения каласса A
+
+class B extends A {
+    int k;
+
+    void showk () {
+        System.out.println (" k : " + k);
+    }
+
+    void sum() {
+        System.out.println("i=j=k: "+(i+j+k));
+
+    }
+}
+
+
+public class SimpleInheritance {
+    public static void main(String[] args) {
+        A superOb = new A();
+        B subOb = new B ();
+
+        // суперкласс может использоваться самостоятельно
+
+        superOb.i = 10;
+        superOb.j = 20;
+        System.out.println("содержимое пакета superOb: ");
+        superOb.showij();
+        System.out.println();
+
+        // подкласс имеет доступ ко всем открытым членам своего суперкласса
+
+        subOb.i = 7;
+        subOb.j = 8;
+        subOb.k = 9;
+        System.out.println("содержимое пакета subOb: ");
+        subOb.showij();
+        subOb.showk();
+
+        System.out.println();
+        System.out.println("Сумма i, j и k:  в объекте subOb");
+    }
+}
