1d0bfcbd566d03069446ef679383564c914be244
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-08-12 19:15:52 +0300

    simple test TableMath

---
+++ src/main/java/com/kirget/javacore/chapter09/MyPack/TableMath.java
@@ -0,0 +1,11 @@
+package com.kirget.javacore.chapter09.MyPack;
+
+public class TableMath {
+    public static void main(String[] args) {
+        for ( int i = 1; i <10; i++)
+    for ( int k = 1; k < 10; k++) {
+        System.out.println(i + "*" + k + "=" + i * k);
+    }
+
+    }
+}
