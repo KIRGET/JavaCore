78a4c8560d422009135a65e9adcca5914a1cce5e
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-08-19 15:05:18 +0300

    test

--- src/main/java/com/kirget/javacore/chapter09/TableMath.java
+++ src/main/java/com/kirget/javacore/chapter09/TableMath.java
@@ -2,10 +2,14 @@ package com.kirget.javacore.chapter09;
 
 public class TableMath {
     public static void main(String[] args) {
-        for ( int i = 1; i <10; i++)
+        for ( int i = 1; i < 10; i++)
     for ( int k = 1; k < 10; k++) {
-        System.out.println(i + "*" + k + "=" + i * k);
+        System.out.println ( i + "*" + k + "=" + i * k );
+
+
     }
 
     }
 }
+
+
