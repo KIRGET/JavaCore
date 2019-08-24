75362b33bdad3c7149e87a9edb72124a66cbdde2
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-08-24 16:52:04 +0300

    game with if & else

--- src/main/java/com/kirget/javacore/SimpleCode/NewGame.java
+++ src/main/java/com/kirget/javacore/SimpleCode/NewGame.java
@@ -1,4 +1,28 @@
 package com.kirget.javacore.SimpleCode;
 
+import static java.lang.System.out;
+import static java.lang.System.setOut;
+
+import java.util.Scanner;
+import java.util.Random;
+
 public class NewGame {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        out.print(" Введите число от 1 до 10:");
+        int inputNumber = keyboard.nextInt();
+        int randomNumber = new Random().nextInt(10) +1;
+
+        if (inputNumber == randomNumber) {
+            out.println("*************");
+            out.println("Вы выиграли. *");
+            out.println("*************");
+        }
+        else {
+            out.println("Вы проиграли");
+            out.println("Случайное число равно");
+            out.println(randomNumber + '*');
+        }
+        out.println("Thanks for the game");
+    }
 }
