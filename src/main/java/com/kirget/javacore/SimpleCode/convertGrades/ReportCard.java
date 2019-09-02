47a1cdaba084220b6721d4a5f79f0ac1659cca95
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-09-02 18:03:03 +0300

    training

--- src/main/java/com/kirget/javacore/SimpleCode/convertGrades/ReportCard.java
+++ src/main/java/com/kirget/javacore/SimpleCode/convertGrades/ReportCard.java
@@ -1,4 +1,58 @@
 package com.kirget.javacore.SimpleCode.convertGrades;
 
-public class convertGrades {
+public class ReportCard {
+    /**
+     Mетод convertGrades принимает один
+     целочисленный аргумент-результат теста
+     и возвращает символ A,B,C or D в зависимости от этого аргумента.
+     */
+
+    public char convertGrades(int testResult) {
+        char grade;
+        if (testResult > 90) {
+            grade = 'A';
+        }
+        else if (testResult >= 80 && testResult < 90) {
+            grade = 'B';
+        }
+        else if(testResult >= 70 && testResult < 80) {
+            grade = 'C';
+        }
+        else {
+            grade = 'D';
+        }
+        return grade;
+    }
+
+//    public static void main(String[] args) {
+//        ReportCard rc = new ReportCard();
+//        char yourGrade = rc.convertGrades(30);
+//        System.out.println("Ваша первая оценка " + yourGrade);
+//        yourGrade = rc.convertGrades(10);
+//        System.out.println("Ваша вторя оценка " + yourGrade);
+//
+//    }
+
+    public static void main(String[] args) {
+        ReportCard rc = new ReportCard();
+        char youGrade = rc.convertGrades(88);
+
+        switch (youGrade) {
+            case 'A': {
+                System.out.println("Превосходная работа");
+                break;
+            }
+            case 'B': {
+                System.out.println("Хорошая работа");
+                break;
+            }
+            case 'C': {
+                System.out.println("Неплохо");
+                break;
+            }
+        }
+
+    }
 }
+
+
