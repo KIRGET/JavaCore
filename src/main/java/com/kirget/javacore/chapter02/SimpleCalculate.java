87c39e2b1e53307d0c854ee70d3d6b056e4ce4b4
Topchienko Kirill <Kirilltopchienko@gmail.com>
2019-11-17 18:19:07 +0300

    + division

--- src/main/java/com/kirget/javacore/chapter02/SimpleCalculate.java
+++ src/main/java/com/kirget/javacore/chapter02/SimpleCalculate.java
@@ -8,6 +8,7 @@ public class SimpleCalculate {
             System.out.println("1. contraction");
             System.out.println("2. subtraction");
             System.out.println("3. multiplicaation");
+            System.out.println("4. division");
             Scanner scanner = new Scanner(System.in);
             int operation = scanner.nextInt();
             System.out.println("input first number");
@@ -19,12 +20,12 @@ public class SimpleCalculate {
                 result = a + b;
             }  else if (operation == 2) {
                 result = a - b;
-            } else {
+            } else if (operation == 3) {
                 result = a * b;
+            } else {
+                result = a / b;
             }
             System.out.println("result = "+ result);
-
         }
-
     }
 
